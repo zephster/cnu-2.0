ff7872818fc12410908b51ff42dd82fdce7b394a
Brandon <antizeph+github@gmail.com>
2011-05-11 19:55:19 -0400

    initial commit just to get it on here.

---
+++ CNU-CS/main.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace CNU_CS
+{
+    partial class main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_lastDownloadedText = new System.Windows.Forms.Label();
+            this.lbl_latestBuildText = new System.Windows.Forms.Label();
+            this.lbl_lastDownloaded = new System.Windows.Forms.Label();
+            this.lbl_latestBuild = new System.Windows.Forms.Label();
+            this.btn_checkUpdate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_lastDownloadedText
+            // 
+            this.lbl_lastDownloadedText.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_lastDownloadedText.Location = new System.Drawing.Point(13, 10);
+            this.lbl_lastDownloadedText.Name = "lbl_lastDownloadedText";
+            this.lbl_lastDownloadedText.Size = new System.Drawing.Size(110, 20);
+            this.lbl_lastDownloadedText.TabIndex = 0;
+            this.lbl_lastDownloadedText.Text = "Last Downloaded:";
+            this.lbl_lastDownloadedText.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lbl_latestBuildText
+            // 
+            this.lbl_latestBuildText.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_latestBuildText.Location = new System.Drawing.Point(13, 30);
+            this.lbl_latestBuildText.Name = "lbl_latestBuildText";
+            this.lbl_latestBuildText.Size = new System.Drawing.Size(110, 20);
+            this.lbl_latestBuildText.TabIndex = 0;
+            this.lbl_latestBuildText.Text = "Latest Build:";
+            this.lbl_latestBuildText.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lbl_lastDownloaded
+            // 
+            this.lbl_lastDownloaded.AutoSize = true;
+            this.lbl_lastDownloaded.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_lastDownloaded.Location = new System.Drawing.Point(119, 12);
+            this.lbl_lastDownloaded.Name = "lbl_lastDownloaded";
+            this.lbl_lastDownloaded.Size = new System.Drawing.Size(56, 16);
+            this.lbl_lastDownloaded.TabIndex = 0;
+            this.lbl_lastDownloaded.Text = "000000";
+            this.lbl_lastDownloaded.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lbl_latestBuild
+            // 
+            this.lbl_latestBuild.AutoSize = true;
+            this.lbl_latestBuild.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_latestBuild.Location = new System.Drawing.Point(119, 32);
+            this.lbl_latestBuild.Name = "lbl_latestBuild";
+            this.lbl_latestBuild.Size = new System.Drawing.Size(56, 16);
+            this.lbl_latestBuild.TabIndex = 0;
+            this.lbl_latestBuild.Text = "000000";
+            this.lbl_latestBuild.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // btn_checkUpdate
+            // 
+            this.btn_checkUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_checkUpdate.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_checkUpdate.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.btn_checkUpdate.Location = new System.Drawing.Point(16, 53);
+            this.btn_checkUpdate.Name = "btn_checkUpdate";
+            this.btn_checkUpdate.Size = new System.Drawing.Size(162, 36);
+            this.btn_checkUpdate.TabIndex = 1;
+            this.btn_checkUpdate.Text = "Check for Update";
+            this.btn_checkUpdate.UseVisualStyleBackColor = true;
+            this.btn_checkUpdate.Click += new System.EventHandler(this.btn_checkUpdate_Click);
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(194, 376);
+            this.Controls.Add(this.btn_checkUpdate);
+            this.Controls.Add(this.lbl_latestBuild);
+            this.Controls.Add(this.lbl_latestBuildText);
+            this.Controls.Add(this.lbl_lastDownloaded);
+            this.Controls.Add(this.lbl_lastDownloadedText);
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "main";
+            this.Padding = new System.Windows.Forms.Padding(10);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CNU 2.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_lastDownloadedText;
+        private System.Windows.Forms.Label lbl_latestBuildText;
+        private System.Windows.Forms.Label lbl_lastDownloaded;
+        private System.Windows.Forms.Label lbl_latestBuild;
+        private System.Windows.Forms.Button btn_checkUpdate;
+
+    }
+}
+
---
+++ CNU-CS/main.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+using System.Net;
+
+//http://www.csharp-examples.net/download-files/
+//http://www.developerfusion.com/article/4637/retrieving-http-content-in-net/2/
+//http://www.csharp-examples.net/xml-nodes-by-name/
+//http://support.microsoft.com/kb/307643
+
+/*
+ * ideas:
+ *      option to auto-check on startup
+ *      keep x number of past copies
+ *      auto-unzip
+ *      update checker/downloader/updater
+ */
+
+
+namespace CNU_CS
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent();
+        }
+
+        private void checkForUpdate()
+        {
+            //retreives LATEST file. normal binary file. just get value.
+            //set var with LATEST version
+        }
+
+        /// <summary>
+        /// 	System.Xml.XmlDocument doc = new XmlDocument() ;
+	    ///     doc.Load("http://www.eggheadcafe.com/rss.xml");
+	    ///     Console.WriteLine(doc.OuterXml );
+	    ///     Console.ReadLine();
+        /// </summary>
+        private void viewChangelog()
+        {
+            //retreives XML for the changelog from current version in LATEST
+        }
+
+        private void downloadUpdate()
+        {
+            //actually download the update, and show progress bar and speed if possible
+        }
+
+        private void btn_checkUpdate_Click(object sender, EventArgs e)
+        {
+            string latest = null;
+            Uri latest_url = new Uri("http://build.chromium.org/f/chromium/snapshots/chromium-rel-xp/LATEST");
+            btn_checkUpdate.Text = "Checking...";
+            btn_checkUpdate.Enabled = false;
+
+            var client = new WebClient();
+            test = client.DownloadStringAsync(latest_url);
+
+
+
+            
+
+
+            /*
+            try
+            {
+                //make the url a changeable setting in case it ever changes and i dont wanna push a new code update out
+                Uri latest_version = new Uri(latest_url);
+                WebRequest request = WebRequest.Create(latest_version);
+                WebResponse response = request.GetResponse();
+                Stream stream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(stream);
+                latest = reader.ReadToEnd();
+            }
+            catch (Exception err)
+            {
+                throw new Exception("Error checking for update: " + err.Message, err);
+            }
+            
+
+            lbl_lastDownloaded.Text = latest;*/
+            btn_checkUpdate.Enabled = true;
+            btn_checkUpdate.Text = "Check for Update";
+        }
+    }
+}
